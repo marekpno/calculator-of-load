6199d1069c3a3c188fa0305ba2e8901d289462bc
marekpno <marekpno@gmail.com>
2023-11-04 23:10:42 +0100

    realised pep8 + added more function

--- cal_of_load.py
+++ cal_of_load.py
@@ -11,7 +11,7 @@ def norm_dimension_pl(total_length=16500, king_to_rear=12000):
 # load from pl homologation
 
 
-def norm_load_pl(dmc=40, axis_3=24):
+def norm_load_pl(dmc=40000, axis_3=24000):
     dmc = ["dmc", 0, dmc]
     axis = ["max_load_axis", 0, axis_3]
     return dmc, axis
@@ -49,9 +49,9 @@ class SemiTrailer:
 
     def matrix_frame(self):
         r_c = ["Rc", self.king_pin, 0]
-        r_d = ["Rd", self.axies, self.axies_load]
-        r_e = ["Re", self.body_load, self.body_load_x]
-        r_f = ["Rf", self.frame_load, self.frame_load_x]
+        r_d = ["Rd", self.axies_load, self.axies]
+        r_e = ["Re", self.body_load_x, self.body_load]
+        r_f = ["Rf", self.frame_load_x, self.frame_load]
         length = ["L", self.length, 0]
         return r_c, r_d, r_e, r_f, length
 
@@ -72,10 +72,12 @@ class Load:
     #    f = [chr(Load.i), self.x, self.load]
     #    return f
 
-#norm_dimension_pl= input("sdfsdf")
-#norm_load_pl= input("sdfsdf")
+# norm_dimension_pl= input("sdfsdf")
+# norm_load_pl= input("sdfsdf")
 
-#for class TractorUnit:
+# for class TractorUnit:
+
+
 '''
 asix_a= int(input("asix_a_tractorunit-->"))
 load_a= int(input("load_a_tractorunit-->"))
@@ -84,13 +86,15 @@ load_b= int(input("load_b_tractorunit-->"))
 saddle= int(input("saddle_tractorunit-->"))
 length= int(input("length_tractorunit-->"))
 '''
-#for class TractorUnit: RANDOM VALUE!
-asix_a= -3150
-load_a=4885
-asix_b= -1220
-load_b=1725
-saddle= - 500
-length=asix_a + asix_b
+
+
+# for class TractorUnit: RANDOM VALUE!
+asix_a_0 = -3150
+load_a_0 = 4885
+asix_b_0 = -1220
+load_b_0 = 1725
+saddle_0 = - 100
+length_0 = asix_a_0 + asix_b_0
 
 '''
 #for class SemiTrailer:
@@ -104,79 +108,87 @@ frame_load_x= int(input("frame_load_x_semitrailer--->"))
 length= int(input("length_semitrailer--->"))
 '''
 
-#for class SemiTrailer for EXAMPLE!!!
-king_pin= 0
-axies= 7700
-axies_load= 2400
-body_load= 3100
-body_load_x= 5300
-frame_load= 1600
-frame_load_x= 5000
-length= 10000
+# for class SemiTrailer for EXAMPLE!!!
+king_pin_0 = 0
+axies_0 = 7700
+axies_load_0 = 1950
+body_load_0 = 3100
+body_load_x_0 = 5300
+frame_load_0 = 2050
+frame_load_x_0 = 5000
+length_frame_0 = 10000
 
 extra_loads = []
 i = 0
-while input("extra load = y, dismiss = n-->")== 'y':
-  mass_of_load = int(input("mass_of_load_semitrailer--->"))
-  mass_of_load_x = int(input("mass_of_load_x_semitrailer--->"))
-  name = f'extra_load_+{i}'
+while input("extra load = y, dismiss = n-->") == 'y':
+    mass_of_load = int(input("mass_of_load_semitrailer--->"))
+    mass_of_load_x = int(input("mass_of_load_x_semitrailer--->"))
 
-  name = Load(mass_of_load, mass_of_load_x)
-  name_force = name.force()
-  extra_loads.append(name_force)
-  i =i+ 1
+    name = Load(mass_of_load, mass_of_load_x)
+    name_force = name.force()
+    extra_loads.append(name_force)
+    i = i + 1
 
-
-
-#without load
-#Ro = axies_load=, Rs = body_load=, Rr = frame_load=
-#czop = 0 ,    R*Rox= Mo *Mox + Ms * Msx + Mr * Mrx
-#R = ( Mo *Mox + Ms * Msx + Mr * Mrx)/R0x
-R_axies = (axies * axies_load + body_load * body_load_x + frame_load * frame_load_x)/axies
-print(f'osie bez ladunku = {int(R_axies)}')
-#0 = R_king + axies_load + body_load + frame_load - R_axies
-R_king = axies_load + body_load + frame_load - R_axies
-print(f'czop bez ladunku = {int(R_king)}')
+# without load
+# Ro = axies_load=, Rs = body_load=, Rr = frame_load=
+# czop = 0 ,    R*Rox= Mo *Mox + Ms * Msx + Mr * Mrx
+# R = ( Mo *Mox + Ms * Msx + Mr * Mrx)/R0x
+R_axies_0 = (axies_0 * axies_load_0 + body_load_0 * body_load_x_0 + frame_load_0 * frame_load_x_0)/axies_0
+print(f'osie bez ladunku = {int(R_axies_0)}')
+# 0 = R_king + axies_load + body_load + frame_load - R_axies
+R_king_0 = axies_load_0 + body_load_0 + frame_load_0 - R_axies_0
+print(f'czop bez ladunku = {int(R_king_0)}')
 # legth homo
 norm_dimension_pl_1 = norm_dimension_pl()
 # print(norm_dimension_pl_1)
 
-#with_load
-#Ro = axies_load=, Rs = body_load=, Rr = frame_load=
-#czop = 0 ,    R*Rox= Mo *Mox + Ms * Msx + Mr * Mrx+
-#R = ( Mo *Mox + Ms * Msx + Mr * Mrx)/R0x
+# with_load
+# Ro = axies_load=, Rs = body_load=, Rr = frame_load=
+# czop = 0 ,    R*Rox= Mo *Mox + Ms * Msx + Mr * Mrx+
+# R = ( Mo *Mox + Ms * Msx + Mr * Mrx)/R0x
 
 for added_load in extra_loads:
-  R_axies = R_axies + (added_load[1]*added_load[2]//axies)
+    R_axies_0 = R_axies_0 + (added_load[1]*added_load[2]//axies_0)
 
 
-print(f'osie z ladunkiem = {int(R_axies)}')
-#0 = R_king + axies_load + body_load + frame_load - R_axies
-R_king = axies_load  + body_load  + frame_load - R_axies
+print(f'osie z ladunkiem = {int(R_axies_0)}')
+# 0 = R_king + axies_load + body_load + frame_load - R_axies
+R_king_0 = axies_load_0 + body_load_0 + frame_load_0 - R_axies_0
 for added_load in extra_loads:
-  R_king = R_king + added_load[2]
-print(f'czop z ladunkiem = {int(R_king)}')
+    R_king_0 = R_king_0 + added_load[2]
+print(f'czop z ladunkiem = {int(R_king_0)}')
 
-R_king = ['R_king',0,R_king]
-R_axies = ['R_axies',axies, R_axies]
 
+# Ma = 0   0 = (a-b)*loab_b -(a-b)Rx + (a-b-saddle)R_king
+
+tractor_axis_b_load = ((asix_a_0-asix_b_0) * load_b_0 + (asix_a_0-asix_b_0-saddle_0)*R_king_0)/(asix_a_0-asix_b_0)
+print(f'os tylna tracotra z ladunkiem= {int(tractor_axis_b_load)}')
+
+# 0 = Fa + Fb - Czop - Fb0 - Fa0 --> Fa = -Fb + czop + Fbo +Fao
+tractor_axis_a_load = - tractor_axis_b_load + R_king_0 + load_b_0 + load_a_0
+print(f'os przednia tracotra z ladunkiem= {int(tractor_axis_a_load)}')
+
+
+R_king = ['R_king', 0, R_king_0]
+R_axies = ['R_axies', axies_0, R_axies_0]
+tractor_axis_b_load = ['tractor_axis_b_load', tractor_axis_b_load, asix_b_0]
+tractor_axis_a_load = ['tractor_axis_a_load', tractor_axis_a_load, asix_a_0]
 # max load
 dmc_0 = norm_load_pl()
 axis_0 = norm_load_pl()[1]
 
 
 # tractorUnit
-tractorunit = TractorUnit(asix_a, load_a, asix_b, load_b, saddle, length)
+tractorunit = TractorUnit(asix_a_0, load_a_0, asix_b_0, load_b_0, saddle_0, length_0)
 matrix = tractorunit.matrix()
 
 # semitrailer
-frame_1 = SemiTrailer(king_pin, axies, axies_load, body_load, body_load_x,
-                      frame_load, frame_load_x, length)
+frame_1 = SemiTrailer(king_pin_0, axies_0, axies_load_0, body_load_0, body_load_x_0,
+                      frame_load_0, frame_load_x_0, length_0)
 matrix_1 = frame_1.matrix_frame()
 
 # extra force
-#extra_load_0 = Load(mass_of_load, mass_of_load_x)
-
+# e xtra_load_0 = Load(mass_of_load, mass_of_load_x)
 
 data = []
 for cell in matrix_1:
@@ -191,14 +203,16 @@ for cell in extra_loads:
     data.append(cell)
 data.append(R_king)
 data.append(R_axies)
+data.append(tractor_axis_b_load)
+data.append(tractor_axis_a_load)
 
 
 df = pd.DataFrame(data=data, columns=['nazwa', 'polozenie', 'wartosc_sily'])
 # df.reset_index(inplace=True)
 #
-#print(df)
+# print(df)
 
 wb = Workbook()
 ws = wb.active
 df.to_excel("nazwa_pliku.xlsx", index=False)
-#wb.save("calculator_of_load.xlsx")
+# wb.save("calculator_of_load.xlsx")
